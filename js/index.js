d7e9216cc3cd40fad4fb6d69f90dfe722e71356a
Brandon-Harris <brandonharris177@gmail.com>
2019-08-06 12:02:24 -0600

    elements_selected

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const homeBtn = document.getElementById("home");
+homeBtn.addEventListener('click', homeClick)
+
+function homeClick(e){
+    console.log(`Its Working!`)
+}
+
+
+const aboutUsBtn = document.getElementById("about-us");
+const blogBtn = document.getElementById("blog");
+const contatUsBtn = document.getElementById("contact-us");
+
+const mapPic = document.getElementById("map-pic");
+
+const waterwayPic = document.getElementById("waterway-pic");
+
+const covePic = document.getElementById("cove-pic");
+
+const funInSunBtn = document.getElementById("fun-in-sun");
+
+const mountainBtn = document.getElementById("mountain-btn");
+
+
+const islandBtn = document.getElementById("island-btn");

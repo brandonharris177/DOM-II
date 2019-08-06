fe576ffffa2ba7f9d6745c44e8002c9f9d42bc5a
Brandon-Harris <brandonharris177@gmail.com>
2019-08-06 14:40:15 -0600

    four-done

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ const homeBtn = document.getElementById("home");
 homeBtn.addEventListener('click', homeClick)
 
 function homeClick(e){
-    console.log(`Its Working!`)
+    // console.log(`Its Working!`)
     const goHome = document.createElement('a');
     goHome.textContent = "Go Home!";
     goHome.style.fontWeight = "bold";
@@ -16,7 +16,7 @@ const aboutUsBtn = document.getElementById("about-us");
 aboutUsBtn.addEventListener('click', aboutClick)
 
 function aboutClick(e){
-    console.log(`Its Working!`)
+    // console.log(`Its Working!`)
     const mysteryPic = document.createElement('img');
     const mysteryText = document.createElement('p')
     mysteryPic.src = "img/mystery.jpg";
@@ -38,9 +38,29 @@ const waterwayPic = document.getElementById("waterway-pic");
 
 const covePic = document.getElementById("cove-pic");
 
+covePic.addEventListener('mouseenter', covePicEnter)
+covePic.addEventListener('mouseleave', covePicLeave)
+
+contentDestination = document.querySelector(".content-destination");
+
+const akbar = document.createElement('p');
+    akbar.textContent = "Its a Trap!!!";
+    akbar.style.fontWeight = "bold";
+    akbar.style.fontSize = "8rem";
+
+function covePicEnter(e){
+    console.log(`Its Working!`)
+    akbar.style.display = 'flex';
+    contentDestination.append(akbar);
+}
+
+function covePicLeave(e){
+    console.log(`Its Still Working!`)
+    akbar.style.display = 'none';
+}
+
 const funInSunBtn = document.getElementById("fun-in-sun");
 
 const mountainBtn = document.getElementById("mountain-btn");
-
 
 const islandBtn = document.getElementById("island-btn");
